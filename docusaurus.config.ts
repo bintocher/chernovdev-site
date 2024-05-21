5071ba9c5bfa53d9c951fd1805141fa28129e3a7
Stanislav Chernov <bintocher@yandex.ru>
2024-05-21 13:09:16 +0300

    изменения архитектуты, замена js на typescript

---
+++ docusaurus.config.ts
@@ -0,0 +1,167 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+
+const config: Config = {
+  title: 'ChernovDev',
+  tagline: 'Сайт про биай, в основном Qlik Sense, а так же личный блог',
+  favicon: 'img/favicon.ico',
+
+  staticDirectories: ['public', 'static'],
+
+  // Set the production url of your site here
+  url: 'https://chernodev.ru',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'bintocher', // Usually your GitHub org/user name.
+  projectName: 'chernovdev-site', // Usually your repo name.
+
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  plugins: [
+    ['docusaurus-plugin-yandex-metrica', { counterID: '97252645' }]
+    , 'docusaurus-plugin-image-zoom'
+    , [require.resolve('docusaurus-lunr-search'),
+    {
+      languages: ['ru', 'en'],
+      highlightResult: true,
+      maxHits: 10,
+    }]
+  ],
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/bintocher/chernovdev-site/blob/main',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'ChernovDev',
+      logo: {
+        alt: 'Qlik-lib Logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Qlik docs',
+        },
+        { to: '/docs/tags', label: 'Tags', position: 'left' },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        // {
+        //   type: 'localeDropdown',position: 'right',
+        // },
+        {
+          href: 'https://github.com/bintocher/chernovdev-site',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      // links: [
+      //   {
+      //     title: 'Docs',
+      //     items: [
+      //       {
+      //         label: 'Tutorial',
+      //         to: '/docs/intro',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: 'Community',
+      //     items: [
+      //       {
+      //         label: 'Stack Overflow',
+      //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+      //       },
+      //       {
+      //         label: 'Discord',
+      //         href: 'https://discordapp.com/invite/docusaurus',
+      //       },
+      //       {
+      //         label: 'Twitter',
+      //         href: 'https://twitter.com/docusaurus',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: 'More',
+      //     items: [
+      //       {
+      //         label: 'Blog',
+      //         to: '/blog',
+      //       },
+      //       {
+      //         label: 'GitHub',
+      //         href: 'https://github.com/facebook/docusaurus',
+      //       },
+      //     ],
+      //   },
+      // ],
+      copyright: `Copyright © 2023 - ${new Date().getFullYear()} Stanislav Chernov`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+    zoom: {
+      selector: '.markdown :not(em) > img',
+      config: {
+        // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
+        background: {
+          light: 'rgb(255, 255, 255)',
+          dark: 'rgb(50, 50, 50)'
+        }
+      }
+    }, metadata: [
+      {
+        name: 'keywords',
+        content: 'qlik sense, help qliksense, qliksense help, qlik, help, библиотека, клик, скачать клик, триал клик, кластер клик, qlik sense multinode'
+      },
+      // {name: 'twitter:card', content: 'summary_large_image'},
+    ],
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import clsx from 'clsx';
-// import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import Heading from '@theme/Heading';
+
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -19,13 +18,8 @@ function HomepageHeader() {
         {/* <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/qs/about">
-            Документация по Qlik ⏱️
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
-            to="/blog">
-            Личный блог
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div> */}
       </div>
@@ -33,7 +27,7 @@ function HomepageHeader() {
   );
 }
 
-export default function Home() {
+export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
